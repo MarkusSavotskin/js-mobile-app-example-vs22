43af55ff16d37129d4f29cad9f5af05fb18785ec
MarkusSavotskin <markus.savotskin@voco.ee>
2023-10-04 10:45:22 +0300

    Added Google OAuth API as login option

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native";
+import Config from "react-native-config";
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import Splash from "./src/screens/auth/Splash";
 import SignUp from "./src/screens/auth/Signup";
+
+
 const App = () => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+      webClientId: Config.GOOGLE_WEB_CLIENT_ID,
+      offlineAccess: true, 
+      iosClientId: Config.GOOGLE_IOS_CLIENT_ID,
+    });
+  }, [])
   return (
     <SafeAreaView>
       <SignUp/>
--- src/components/GoogleLogin/index.js
+++ src/components/GoogleLogin/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { Image, TouchableOpacity } from 'react-native';
+import {Image, TouchableOpacity} from 'react-native';
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
 
-import { styles } from './styles'
+import {styles} from './styles';
 
 const GoogleLogin = () => {
-    return (
-        <TouchableOpacity activeOpacity={0.6} style={styles.container}>
-            <Image source style={styles.image} source={require('../../assets/Google.png')}/>
-        </TouchableOpacity>
-    )
-}
-export default React.memo(GoogleLogin)
+  signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      //setState({userInfo});
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
+  return (
+    <TouchableOpacity activeOpacity={0.6} onPress={signIn} style={styles.container}>
+      <Image
+        source
+        style={styles.image}
+        source={require('../../assets/Google.png')}
+      />
+    </TouchableOpacity>
+  );
+};
+export default React.memo(GoogleLogin);
