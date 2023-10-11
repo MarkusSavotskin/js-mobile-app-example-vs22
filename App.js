8ca83f7fdd735744680ecae45b12adeb9c11c636
MarkusSavotskin <markus.savotskin@voco.ee>
2023-10-11 09:11:08 +0300

    Create navigation between screens and tabs

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, View, Image} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {NavigationContainer} from '@react-navigation/native';
@@ -25,7 +25,32 @@ const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let icon
+
+        if (route.name === 'Home') {
+          icon = focused
+          ? require('./src/assets/tabs/home_active.png')
+          : require('./src/assets/tabs/home.png')
+        } else if (route.name === 'Favorites') {
+          icon = focused
+          ? require('./src/assets/tabs/bookmark_active.png')
+          : require('./src/assets/tabs/bookmark.png')
+      } else if (route.name === 'Profile') {
+        icon = focused
+        ? require('./src/assets/tabs/profile_active.png')
+        : require('./src/assets/tabs/profile.png')
+      }
+
+      return <Image style={{width: 24, height: 24}} source={icon}/>
+    },
+    headerShown: false,
+    tabBarShowLabel: false,
+    tabBarStyle: {borderTopColor: colors.lightGrey}
+    })}
+    >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Favorites" component={Favorites} />
       <Tab.Screen name="Profile" component={Profile} />
